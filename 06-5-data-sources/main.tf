bda868c9fc64f7f5e6f051d54e4c48b0b5879fff
Timotej Avsec <avsectimotej1@gmail.com>
2022-11-27 17:00:18 +0100

    Add data-source

---
+++ 06-5-data-sources/main.tf
@@ -0,0 +1,14 @@
+# Find the latest available AMI that is tagged with Component = web
+data "aws_ami" "web" {
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "tag:Component"
+    values = ["web"]
+  }
+
+  most_recent = true
+}
