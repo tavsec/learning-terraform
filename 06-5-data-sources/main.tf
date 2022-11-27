85beb2582541938b02f1913b08031c83f79c2038
Timotej Avsec <avsectimotej1@gmail.com>
2022-11-27 17:07:06 +0100

    Add example for latest Ubuntu AMI data source

--- 06-5-data-sources/main.tf
+++ 06-5-data-sources/main.tf
@@ -12,3 +12,13 @@ data "aws_ami" "web" {
 
   most_recent = true
 }
+
+# Find the latest AMI for Ubuntu server
+data "aws_ami" "latest_ubuntu20" {
+    owners = [ "099720109477" ]
+    most_recent = true
+    filter {
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+}
