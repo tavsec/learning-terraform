5dc69b14d72388639710bec97e1f5456b02f4717
Timotej Avsec <avsectimotej1@gmail.com>
2022-11-27 18:57:25 +0100

    Add remote-exec example

---
+++ 9-remote-exec/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "web" {
+  # ...
+
+  # Establishes connection to be used by all
+  # generic remote provisioners (i.e. file/remote-exec)
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = var.root_password
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "puppet apply",
+      "consul join ${aws_instance.web.private_ip}",
+    ]
+  }
+}
